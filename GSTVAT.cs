666370e9eb40a10657c67f5cc906135513ddbf30
VishalJR <101104675+VishalJR@users.noreply.github.com>
2022-03-16 21:45:42 +0530

    Update GSTVAT.cs

--- GSTVAT.cs
+++ GSTVAT.cs
@@ -1,34 +1,32 @@
 using System;
-class caltax1
+class VAT
 {
-public void calVAT()
+public virtual void calculateTax(int price)
+{
+int value;
+value=price*20/100;
+Console.WriteLine("VAT:"+value);
+}
+}
+class GST:VAT
+{
+public override void calculateTax(int price)
+{
+int value;
+value=price*12/100;
+Console.WriteLine("GST:"+value);
+}
+}
+class Inherit
+{
+public static void Main()
 {
 int price;
-float VAT,pt;
-Console.WriteLine("Price:");
-VAT=(price*20)/100;
-pt=VAT+price;
-Console.WriteLine("Total Amount after VAT:"+pt);
+Console.WriteLine("price is:");
+price=Convert.ToInt32(Console.ReadLine());
+VAT obj=new VAT();
+obj.calculateTax(price);
+GST obj1=new GST();
+obj1.calculateTax(price);
 }
 }
-class caltax2:caltax1
-{
-public void calGST(int price)
-{
-float GST,pt0;
-GST=(price*12)/100;
-pt0=GST+price;
-Console.WriteLine("Total Amount after GST:"+pt0);
-}
-}
-class inherit1
-{
-public static void main()
-{
-caltax1 obj=new caltax1();
-caltax2 obj1=new caltax2();
-obj.calVAT();
-obj1.calGST(price);
-}
-}
-
