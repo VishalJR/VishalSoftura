e0a33328a6de6ab66725693c60d635b7258ce2a1
Vishal <101104675+VishalJR@users.noreply.github.com>
2022-05-03 19:54:51 +0530

    Add files via upload

---
+++ 03-05-2022/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdoMVC.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string EmailID { get; set; }
+        public string MobileNo { get; set; }
+    }
+}
---
+++ 03-05-2022/CustomerDAL.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Data.SqlClient;
+using AdoMVC.Models;
+using System.Data;
+
+namespace AdoMVC.DAL
+{
+    public class CustomerDAL
+    {
+        public string cnn = "";
+
+
+        public CustomerDAL()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath
+                (Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            cnn = builder.GetSection("ConnectionStrings:Conn").Value;
+        }
+
+        public List<Customer> GetAllCustomers()
+        {
+            List<Customer> listCustomer = new List<Customer>();
+            using (SqlConnection con = new SqlConnection(cnn))
+            {
+                using (SqlCommand cmd = new SqlCommand("GetAllCustomers", con))
+                {
+                    if (con.State == ConnectionState.Closed)
+                        con.Open();
+                    IDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        listCustomer.Add(new Customer()
+                        {
+                            CustomerID = int.Parse(reader["CustomerID"].ToString()),
+                            CustomerName = reader["CustomerName"].ToString(),
+                            EmailID = reader["EmailID"].ToString(),
+                            MobileNo = reader["MobileNo"].ToString()
+                        }); ;
+                    }
+
+                }
+            }
+            return listCustomer;
+        }
+
+
+
+
+
+
+
+
+    }
+}
